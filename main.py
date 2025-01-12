7a1090551faabb9991d7aae48c453cf5cb9b76dc
rianaditro <rianaditro@gmail.com>
2025-01-12 11:01:41 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+import time
+import schedule
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',
+)
+
+
+def job():
+    print("Executing job")
+    logging.info('Job executed')
+
+def main():
+    schedule.every(4).seconds.do(job)
+
+    schedule.run_all()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        print("Waiting...")
+
+
+if __name__ == "__main__":
+    main()
